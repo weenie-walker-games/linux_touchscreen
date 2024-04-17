107dd43b517a016b0498d70139dd9b9e218ec764
weenie-walker-games <weenie.walker.games@gmail.com>
2024-04-16 23:17:17 -0400

    Continuing the lesson with labels

--- window.py
+++ window.py
@@ -1,9 +1,23 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
-# Taken from RealPython website
+# Taken from RealPython website, https://realpython.com/python-gui-tkinter/
 
 window = tk.Tk()
-label = tk.Label(text="Python Rocks!")
+label = tk.Label(
+    text="Python Rocks!",
+    foreground="white",  # Set the text color to white
+    background="black",  # Set the background color to black
+    )
 label.pack()
+
+label2 = tk.Label(
+    text="Try two",
+    bg="#34A2FE",
+    fg="white",
+    width=10,
+    height=10,
+    )
+label2.pack()
 
 window.mainloop()
