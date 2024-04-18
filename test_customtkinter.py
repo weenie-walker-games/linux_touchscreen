0dfbf12329129d32886b26f7706fb3e7178a8dc3
Adam Bradley <weenie.walker.games@gmail.com>
2024-04-18 10:47:22 -0400

    Corrected some missing parameters

--- test_customtkinter.py
+++ test_customtkinter.py
@@ -17,18 +17,18 @@ frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
 label = ctk.CTkLabel(master=frame, text="Login System")
-label.pack()
+label.pack(pady=12, padx=10)
 
 entry1 = ctk.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack()
+entry1.pack(pady=12, padx=10)
 
 entry2 = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
-entry2.pack()
+entry2.pack(pady=12, padx=10)
 
 button = ctk.CTkButton(master=frame, text="Login", command=login)
-button.pack()
+button.pack(pady=12, padx=10)
 
 checkbox = ctk.CTkCheckBox(master=frame, text="Remember Me")
-checkbox.pack()
+checkbox.pack(pady=12, padx=10)
 
 root.mainloop()
