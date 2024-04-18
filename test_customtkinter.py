9d65fd6b4c2b23b8b56ce78a64d83dc7b6f14b94
Adam Bradley <weenie.walker.games@gmail.com>
2024-04-18 10:59:27 -0400

    Added a slider/progress bar example

--- test_customtkinter.py
+++ test_customtkinter.py
@@ -6,11 +6,15 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
 root = ctk.CTk()
-root.geometry("500x350")
+root.geometry("500x500")
 
 
 def login():
     print("Test")
+
+
+def slider_callback(value):
+    progressbar_1.set(value)
 
 
 frame = ctk.CTkFrame(master=root)
@@ -18,6 +22,9 @@ frame.pack(pady=20, padx=60, fill="both", expand=True)
 
 label = ctk.CTkLabel(master=frame, text="Login System")
 label.pack(pady=12, padx=10)
+
+progressbar_1 = ctk.CTkProgressBar(master=frame)
+progressbar_1.pack(pady=12, padx=10)
 
 entry1 = ctk.CTkEntry(master=frame, placeholder_text="Username")
 entry1.pack(pady=12, padx=10)
@@ -27,6 +34,10 @@ entry2.pack(pady=12, padx=10)
 
 button = ctk.CTkButton(master=frame, text="Login", command=login)
 button.pack(pady=12, padx=10)
+
+slider_1 = ctk.CTkSlider(master=frame, command=slider_callback, from_=0, to=1)
+slider_1.pack(pady=12, padx=10)
+slider_1.set(0.5)
 
 checkbox = ctk.CTkCheckBox(master=frame, text="Remember Me")
 checkbox.pack(pady=12, padx=10)
